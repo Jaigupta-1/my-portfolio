90d6a0d1ec50681e312c87b45319ad1f816c0456
jaig <jai.gupta@mygstcafe>
2026-05-15 15:53:15 +0530

    v

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,8 @@ export const About = () => {
   return (
     <div className="h-screen pt-[10vh]">
       <p className="font-medium text-2xl text-center text-white">About</p>
-      <div className="bg-[#202733] flex flex-col text-white sm:flex-row   h-[85vh] ">
-        <div className="flex-1 max-w-2xl flex flex-col justify-center px-5 gap-8 h-full">
+      <div className="bg-[#202733] flex flex-col text-white sm:flex-row h-[85vh] ">
+        <div className="flex-1 max-w-2xl flex flex-col justify-center p-5 gap-6 h-full">
           <p className="font-medium text-2xl">Know Who I'M</p>
           <p className=" font-light text-lg tracking-wider leading-5 sm:tracking-widest sm:leading-6">
             Hi! I’m <b>Jai Kumar Gupta</b> from Kanpur, India — a passionate
@@ -12,8 +12,8 @@ export const About = () => {
             backend technologies. I enjoy solving real-world problems and
             creating seamless digital experiences.
           </p>
-          <button className=" py-2 px-5 rounded-xl text-black w-auto bg-amber-400 font-medium self-center whitespace-nowrap hover:text-lg">
-            <a href="" download={"./Resume_Jai_2026.pdf"}>
+          <button className=" py-2 px-5 rounded-xl text-black w-auto bg-amber-400 font-medium self-start whitespace-nowrap hover:text-lg">
+            <a href="./Resume_Jai_2026.pdf" download>
               Download CV
             </a>
           </button>
