892a3eb10df55161955caed03f0290e6bca8a4ef
jaig <jai.gupta@mygstcafe>
2026-05-15 16:07:02 +0530

    v

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const OpenMenu = ({ handleFn }) => {
   return (
-    <div className="h-52 w-full absolute top-0 right-0 flex flex-col p-1 gap-1 backdrop-blur-lg rounded ">
+    <div className="h-52 w-full absolute top-0 right-0 flex flex-col p-1 gap-1 backdrop-blur-lg rounded z-50">
       <i className="ri-close-line self-end pr-10 pt-3" onClick={handleFn}></i>
       <div className="pl-5">
         <a href="#home">
@@ -57,9 +57,9 @@ export const Navbar = () => {
   };
   return (
     // bg-[#202733]
-    <div className="  text-white text-xl  h-[10vh] w-full flex justify-between items-center px-10 fixed top-0 left-0 z-50 backdrop-blur-md">
+    <div className="bg-[#202733]  text-white text-xl  min-h-[10vh] w-full flex justify-between items-center px-10 fixed top-0 left-0 z-10 ">
       <div className="tracking-wide text-2xl font-bold">
-        <p>Portfolio</p> 
+        <a href="#home"><p>Portfolio</p></a>
       </div>
       <div className="">
         <ul className="flex ">
