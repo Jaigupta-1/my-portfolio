3e37ad8d891e0f4a8c7b1028863cbedad0df1c16
jaig <jai.gupta@mygstcafe.in>
2026-05-17 14:45:32 +0530

    fd

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,97 +1,4 @@
-// export const Skills = () => {
-//   // const color = "red";
-//   // const shadow = " rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"
-//   return (
-//     <div className="min-h-screen text-white w-full pt-[10vh] ">
-//       <h1 className="text-center font-medium text-xl text-amber-400">Skills</h1>
-//       <p className="my-3 pl-5">Frontend</p>
-//       <ul className={`flex flex-wrap pt-3 bg-[#202733] rounded-md shadow-xl/20 mx-10 space-y-3`}>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-html5-plain colored text-3xl sm:text-6xl"></i>
-//           <li>Html</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-css3-plain colored text-3xl sm:text-6xl"></i>
-//           <li>Css</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-html5-plain colored text-3xl sm:text-6xl"></i>
-//           <li>Html</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-css3-plain colored text-3xl sm:text-6xl"></i>
-//           <li>Css</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-javascript-plain colored text-3xl sm:text-6xl"></i>
-//           <li>JavaScript</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-react-original colored text-3xl sm:text-6xl"></i>
-//           <li>React</li>
-//         </div>
-//       </ul>
-//       <p className="my-3 pl-5">Backend</p>
-
-//       <ul className="flex flex-wrap pt-2 bg-[#202733] rounded-md shadow-xl/20 mx-10 space-y-3">
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-nodejs-plain-wordmark colored text-3xl sm:text-6xl"></i>
-//           <li>NodeJS</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-express-original text-3xl sm:text-6xl"></i>
-//           <li>ExpressJS</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-nodejs-plain-wordmark colored text-3xl sm:text-6xl"></i>
-//           <li>NodeJS</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-express-original text-3xl sm:text-6xl"></i>
-//           <li>ExpressJS</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-mongodb-plain colored text-3xl sm:text-6xl"></i>
-//           <li>MongoDB</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-postgresql-plain colored text-3xl sm:text-6xl"></i>
-//           <li>PostgresSQL</li>
-//         </div>
-//       </ul>
-//       <p className="my-3 pl-5">Devops</p>
-
-//       <ul className="flex flex-wrap pt-2 bg-[#202733] rounded-md inset-shadow-xl mx-10 space-y-3">
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-git-plain colored text-3xl sm:text-6xl"></i>
-//           <li>Git</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-github-original text-3xl sm:text-6xl"></i>
-//           <li>GitHub</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-nodejs-plain-wordmark colored text-3xl sm:text-6xl"></i>
-//           <li>NodeJS</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-express-original text-3xl sm:text-6xl"></i>
-//           <li>ExpressJS</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-docker-plain colored text-3xl sm:text-6xl"></i>
-//           <li>Docker</li>
-//         </div>
-//         <div className="w-1/3 sm:flex-1 text-center">
-//           <i className="devicon-linux-plain colored text-3xl sm:text-6xl"></i>
-//           <li>Linux</li>
-//         </div>
-//       </ul>
-//     </div>
-//   );
-// };
-// // box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-
+import glory from "../../public/glory.png";
 const SkillBar = ({ title, percentage }) => {
   return (
     <div className="w-full sm:w-1/4 mx-12 max-w-sm min-h-20">
@@ -168,7 +75,7 @@ export const Skills: any = () => {
         <SkillBar title="UI/UX Design" percentage={75} />
       </div>
       <div className="relative overflow-hidden rounded-3xl w-full mx-auto py-8">
-        <div className=" absolute inset-0 bg-[url(./glory.png)] bg-cover opacity-80"></div>
+        <div className=" absolute inset-0 bg-cover opacity-80" style={{ backgroundImage: `url(${glory})` }}></div>
         {/* <div className="absolute inset-0 bg-black/40"></div> */}
         <div className="relative flex flex-col sm:flex-row justify-around items-center gap-5 sm:gap-0">
           <div className="font-semibold w-[80%] text-center sm:w-auto text-lg">Let's build something amazing together!</div>
